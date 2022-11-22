faaa770ad91a10f12e0d5687f13badf054d88c9f
tagazok <leplus.olivier@gmail.com>
2022-11-22 13:27:09 +0100

    feat(template): Remove unecessary files and add prerequisites

---
+++ src/ui-components/S3Image.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Storage } from "aws-amplify";
+
+export default function S3Image(props) {
+
+    const style = {
+        width: "491px",
+        height: "auto",
+        display: "block",
+        gap: "unset",
+        alignIitems: "unset",
+        justifyCcontent: "unset",
+        flexShrink: 0,
+        position: "relative",
+        padding: "0px",
+        objectFit: "cover",
+        boxSizing: "content-box",
+        maxWidth: "100%"
+    }
+    const [url, setUrl] = useState("");
+
+    Storage.get(props.id).then(res => {
+        setUrl(res);
+    }).catch(error => {
+        console.log(error);
+    });
+
+    return (
+        <img
+            style={style}
+            src={url}
+            alt="Coucou"
+        />
+    );
+};
